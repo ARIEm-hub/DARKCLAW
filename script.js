f6556a088318f31f57b2d19d2c4ddef8e290a918
ARIEm-hub <karagejruslan@gmail.com>
2026-08-05 08:22:41 +0000

    Fix menu button toggle

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu(){
+
+    const menu = document.querySelector(".menu-content");
+
+    if(menu.style.display === "none"){
+
+        menu.style.display = "block";
+
+    } else {
+
+        menu.style.display = "none";
+
+    }
+
+}
