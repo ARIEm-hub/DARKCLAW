5e9f4992edbba1fd5770af59b4dcd65d39ebd242
ARIEm-hub <karagejruslan@gmail.com>
2026-08-05 13:31:31 +0000

    Added DARKCLAW glitch logo and particles

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
 
     menu.classList.toggle("active");
 }
+
+
+const particles = document.getElementById("particles");
+
+
+for(let i = 0; i < 40; i++){
+
+    let p=document.createElement("span");
+
+    p.className="particle";
+
+
+    p.style.left=Math.random()*100+"vw";
+
+    p.style.animationDuration=
+    (5+Math.random()*10)+"s";
+
+
+    p.style.animationDelay=
+    Math.random()*5+"s";
+
+
+    particles.appendChild(p);
+
+}
