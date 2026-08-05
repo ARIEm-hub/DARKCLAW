ddb3751763cb87f982d7a29701bed1ebbb775c4d
ARIEm-hub <karagejruslan@gmail.com>
2026-08-05 13:38:16 +0000

    Fixed DARKCLAW particles effect

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-function toggleMenu() {
-
-    const menu = document.querySelector(".menu-content");
-
-    menu.classList.toggle("active");
-}
-
-
 const particles = document.getElementById("particles");
 
-
-for(let i = 0; i < 40; i++){
-
-    let p=document.createElement("span");
-
-    p.className="particle";
+console.log("DARKCLAW particles loaded");
 
 
-    p.style.left=Math.random()*100+"vw";
+for(let i = 0; i < 50; i++){
 
-    p.style.animationDuration=
-    (5+Math.random()*10)+"s";
+    let p = document.createElement("span");
 
+    p.className = "particle";
 
-    p.style.animationDelay=
-    Math.random()*5+"s";
+    p.style.left = Math.random() * 100 + "vw";
 
+    p.style.animationDuration =
+    (5 + Math.random() * 10) + "s";
+
+    p.style.animationDelay =
+    Math.random() * 5 + "s";
 
     particles.appendChild(p);
 
